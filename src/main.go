cc584f434d6ade6aee189030faea1486ef598162
Ethan North <northethana@gmail.com>
2025-02-08 11:55:30 -0600

    struct setup

--- src/main.go
+++ src/main.go
@@ -3,15 +3,40 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync"
 
+	"github.com/docker/docker/client"
 	"github.com/gin-gonic/gin"
 )
 
-func createFunction(context *gin.Context) {
+type CreateRequest struct {
+	Name string `json:"name"`
+	Code string `json:"code"`
+}
+
+type InvokeRequest struct {
+	Name   string                 `json:"name"`
+	Params map[string]interface{} `json:"params"`
+}
+
+type FuncDef struct {
+	FuncCode string
+	FuncCntr string
+}
+
+type Engine struct {
+	cntrCli   *client.Client
+	functions map[string]FuncDef
+	netName   string
+	engId     string
+	mu        sync.Mutex
+}
+
+func createHandler(context *gin.Context) {
 	context.IndentedJSON(http.StatusCreated, "Created function")
 }
 
-func invokeFunction(context *gin.Context) {
+func invokeHandler(context *gin.Context) {
 	context.IndentedJSON(http.StatusOK, "Invoked function")
 }
 
@@ -19,8 +44,8 @@ func main() {
 
 	// Setup api routes
 	router := gin.Default()
-	router.POST("/api/functions/v1/create", createFunction)
-	router.POST("/api/functions/v1/invoke", invokeFunction)
+	router.POST("/api/functions/v1/create", createHandler)
+	router.POST("/api/functions/v1/invoke", invokeHandler)
 
 	// Run the api loop
 	fmt.Println("Starting server")
